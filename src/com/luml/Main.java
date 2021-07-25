17e1e899a6fe545033050f4b186097bdf355b4e0
鲁孟良 <lumengliang@xjj.com>
2021-07-25 19:27:59 +0800

    init

---
+++ src/com/luml/Main.java
@@ -0,0 +1,12 @@
+package com.luml;
+
+/**
+ * @author luml
+ * @description
+ * @date 2021/7/25 上午10:09
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(22);
+    }
+}
