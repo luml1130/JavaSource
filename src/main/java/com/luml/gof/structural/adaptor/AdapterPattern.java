55b957798a252e53d6535643fbb30e9d5c6187ff
luml1130 <346792287@qq.com>
2026-03-30 09:39:30 +0800

    适配器模式

---
+++ src/main/java/com/luml/gof/structural/adaptor/AdapterPattern.java
@@ -0,0 +1,36 @@
+package com.luml.gof.structural.adaptor;
+
+// 目标接口（客户端期望的接口）
+interface Target {
+    void request();
+}
+
+// 被适配者（已存在的、接口不兼容的类）
+class Adaptee {
+    public void specificRequest() {
+        System.out.println("特殊请求!");
+    }
+}
+
+// 适配器（实现目标接口，并持有被适配者实例）
+class Adapter implements Target {
+    private Adaptee adaptee;
+
+    public Adapter(Adaptee adaptee) {
+        this.adaptee = adaptee;
+    }
+
+    @Override
+    public void request() {
+        adaptee.specificRequest();
+    }
+}
+
+// 客户端代码
+ class Client {
+    public static void main(String[] args) {
+        Adaptee adaptee = new Adaptee();
+        Target target = new Adapter(adaptee);
+        target.request();
+    }
+}
