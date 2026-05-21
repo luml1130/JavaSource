e211d72bb5049a0fde0d3a3969868c3793924099
luml1130 <346792287@qq.com>
2026-05-21 15:08:14 +0800

    ThreadGroup

--- src/main/java/com/luml/thread/group/StrictThreadGroup.java
+++ src/main/java/com/luml/thread/group/StrictThreadGroup.java
@@ -3,6 +3,9 @@ package com.luml.thread.group;
 /**
  * @author luml
  * @description
+ * 如何使异常影响其他线程？
+ * 虽然默认不影响，但开发者可以通过重写 ThreadGroup 的行为来改变这一机制。
+ * 例如，可以创建一个自定义线程组，当其中任何一个线程发生未捕获异常时，强制中断组内所有其他线程：
  * @date 2026/5/21
  */
 public class StrictThreadGroup extends ThreadGroup{
